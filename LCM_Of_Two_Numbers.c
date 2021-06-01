1dd4e294b16f5d0b63a15091a194607a2f2e9b59
hrprem16 <83371230+hrprem16@users.noreply.github.com>
2021-06-01 14:21:07 +0530

    created LCM_Of_Two_Numbers.c
    Program in C to find the LCM of two numbers using recursion

---
+++ LCM_Of_Two_Numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main()
+{
+	int lcm(int a, int b);
+}
+	lcm()
+{
+	int a=0,b=0;
+	printf("Enter Two Numbers : ");
+	scanf("%d%d", &a,&b);
+	printf("\nThe LCM of %d and %d is %d", a,b,lcm);
+}
